6af5390ab6c646d45b60b06af8f66174bd6e5475
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2025-03-15 10:32:55 -0300

    Cadastro Pessoas: Preview componentes lista de pessoas

---
+++ CadastroPessoas/composeApp/src/androidMain/kotlin/br/edu/utfpr/cadastropessoas/ui/lista/ListaPessoasScreenPreviews.kt
@@ -0,0 +1,77 @@
+package br.edu.utfpr.cadastropessoas.ui.lista
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.utfpr.cadastropessoas.data.model.Endereco
+import br.edu.utfpr.cadastropessoas.data.model.Pessoa
+
+@Preview(showBackground = true)
+@Composable
+private fun ListaPessoasTopBarPreview() {
+    MaterialTheme {
+        ListaPessoasTopBar(
+            mostrarAcaoAtualizar = true,
+            onAtualizar = {}
+        )
+    }
+}
+
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+private fun CarregandoPessoasPreview() {
+    MaterialTheme {
+        CarregandoPessoas()
+    }
+}
+
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+private fun ErroCarregarPessoasPreview() {
+    MaterialTheme {
+        ErroCarregarPessoas(
+            onTentarNovamente = {}
+        )
+    }
+}
+
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+private fun ListaPessoasVaziaPreview() {
+    MaterialTheme {
+        ListaPessoas(
+            pessoas = listOf(),
+            onPessoaSelecionada = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ListaPessoasPreenchidaPreview() {
+    MaterialTheme {
+        ListaPessoas(
+            pessoas = listOf(
+                Pessoa(
+                    id = 1,
+                    nome = "João",
+                    endereco = Endereco(
+                        logradouro = "Tocantins",
+                        numero = 1000,
+                        cidade = "Pato Branco - PR"
+                    )
+                ),
+                Pessoa(
+                    id = 2,
+                    nome = "José",
+                    endereco = Endereco(
+                        logradouro = "Av. Brasil",
+                        numero = 10,
+                        cidade = "Curitiba - PR"
+                    )
+                )
+            ),
+            onPessoaSelecionada = {}
+        )
+    }
+}
